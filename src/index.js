7915dda44d0066a5c20aa6a9570b3ecade567224
ansari-vazir <vaziransari9@gmail.com>
2023-10-19 20:12:31 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react' ;
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let currTime = new Date();
+currTime = currTime.getHours();
+
+let greeting='';
+const greetCol ={ };
+
+if(currTime>=1 && currTime<12){
+  greeting='Good Morning';
+  greetCol.color='green';
+}else if(currTime>=12 && currTime<18){
+  greeting='Good Afternoon';
+  greetCol.color='orange';
+}else{
+  greeting='Good Evening';
+  greetCol.color='blue';
+}
+
+
+ReactDOM.render(
+  <>
+  <div>
+    <h1> Hello Visitor , <span style={greetCol}>{greeting}</span></h1>
+  </div>
+  </>
+
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
